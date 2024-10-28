5d056ef54067d3c4369d3ef7d88842fe5726c86b
ibrakusuma <ibrahimrama3123@gmail.com>
2024-10-28 15:23:47 +0700

    revisi login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,57 +17,19 @@
                         <!--begin::Heading-->
                         <div class="text-center mb-11">
                             <!--begin::Title-->
-                            <h1 class="text-gray-900 fw-bolder mb-3">
-                                Sign In
+                            <h1 class="text-gray-900 fw-bolder mb-1">
+                                Si<span style="color: #49BECB;">Cantik</span>
                             </h1>
                             <!--end::Title-->
 
                             <!--begin::Subtitle-->
                             <div class="text-gray-500 fw-semibold fs-6">
-                                Your Social Campaigns
+                                Masukkan Email dan Password Anda
                             </div>
                             <!--end::Subtitle--->
                         </div>
                         <!--begin::Heading-->
-
-                        <!--begin::Login options-->
-                        <div class="row g-3 mb-9">
-                            <!--begin::Col-->
-                            <div class="col-md-6">
-                                <!--begin::Google link--->
-                                <a href="#"
-                                    class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-                                    <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/google-icon.svg') }}"
-                                        class="h-15px me-3">
-                                    Sign in with Google
-                                </a>
-                                <!--end::Google link--->
-                            </div>
-                            <!--end::Col-->
-
-                            <!--begin::Col-->
-                            <div class="col-md-6">
-                                <!--begin::Google link--->
-                                <a href="#"
-                                    class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
-                                    <img alt="Logo" src="{{ asset('assets/media/svg/brand-logos/apple-black.svg') }}"
-                                        class="theme-light-show h-15px me-3">
-                                    <img alt="Logo"
-                                        src="{{ asset('assets/media/svg/brand-logos/apple-black.svg') }}"
-                                        class="theme-dark-show h-15px me-3">
-                                    Sign in with Apple
-                                </a>
-                                <!--end::Google link--->
-                            </div>
-                            <!--end::Col-->
-                        </div>
-                        <!--end::Login options-->
-
-                        <!--begin::Separator-->
-                        <div class="separator separator-content my-14">
-                            <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
-                        </div>
-                        <!--end::Separator-->
+                        
                         <!--begin::Input group--->
                         <div class="fv-row mb-8 fv-plugins-icon-container">
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
@@ -102,7 +64,7 @@
                             <!--begin::Link-->
                             <a href="#"
                                 class="link-primary">
-                                Forgot Password ?
+                                Lupa Password ?
                             </a>
                             <!--end::Link-->
                         </div>
@@ -124,148 +86,38 @@
                                 <!--end::Indicator progress--> </button>
                         </div>
                         <!--end::Submit button-->
-
-                        <!--begin::Sign up-->
-                        <div class="text-gray-500 text-center fw-semibold fs-6">
-                            Not a Member yet?
-
-                            <a href="#" class="link-primary">
-                                Sign up
-                            </a>
-                        </div>
-                        <!--end::Sign up-->
                     </form>
                 </div>
                 <!--end::Wrapper-->
             </div>
             <!--end::Form-->
-
-            <!--begin::Footer-->
-            <div class="w-lg-500px d-flex flex-stack px-10 mx-auto">
-                <!--begin::Languages-->
-                <div class="me-10">
-                    <!--begin::Toggle-->
-                    <button class="btn btn-flex btn-link btn-color-gray-700 btn-active-color-primary rotate fs-base"
-                        data-kt-menu-trigger="click" data-kt-menu-placement="bottom-start" data-kt-menu-offset="0px, 0px"
-                        fdprocessedid="sc0c4i">
-                        <img data-kt-element="current-lang-flag" class="w-20px h-20px rounded me-3"
-                            src="{{ asset('assets/media/flags/united-states.svg') }}" alt="">
-
-                        <span data-kt-element="current-lang-name" class="me-1">English</span>
-
-                        <span class="d-flex flex-center rotate-180">
-                            <i class="ki-duotone ki-down fs-5 text-muted m-0"></i> </span>
-                    </button>
-                    <!--end::Toggle-->
-
-                    <!--begin::Menu-->
-                    <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-4 fs-7"
-                        data-kt-menu="true" id="kt_auth_lang_menu">
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="English">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="{{ asset('assets/media/flags/united-states.svg') }}" alt="">
-                                </span>
-                                <span data-kt-element="lang-name">English</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="Spanish">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="{{ asset('assets/media/flags/spain.svg') }}" alt="">
-                                </span>
-                                <span data-kt-element="lang-name">Spanish</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="German">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="{{ asset('assets/media/flags/germany.svg') }}" alt="">
-                                </span>
-                                <span data-kt-element="lang-name">German</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="Japanese">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="{{ asset('assets/media/flags/japan.svg') }}" alt="">
-                                </span>
-                                <span data-kt-element="lang-name">Japanese</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                        <!--begin::Menu item-->
-                        <div class="menu-item px-3">
-                            <a href="#" class="menu-link d-flex px-5" data-kt-lang="French">
-                                <span class="symbol symbol-20px me-4">
-                                    <img data-kt-element="lang-flag" class="rounded-1"
-                                        src="{{ asset('assets/media/flags/france.svg') }}" alt="">
-                                </span>
-                                <span data-kt-element="lang-name">French</span>
-                            </a>
-                        </div>
-                        <!--end::Menu item-->
-                    </div>
-                    <!--end::Menu-->
-                </div>
-                <!--end::Languages-->
-
-                <!--begin::Links-->
-                <div class="d-flex fw-semibold text-primary fs-base gap-5">
-                    <a href="#" target="_blank">Terms</a>
-
-                    <a href="#" target="_blank">Plans</a>
-
-                    <a href="#" target="_blank">Contact Us</a>
-                </div>
-                <!--end::Links-->
-            </div>
-            <!--end::Footer-->
         </div>
         <!--end::Body-->
 
         <!--begin::Aside-->
         <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
-            style="background-image: url({{asset('assets/media/misc/auth-bg-2.png')}})">
+            style="background-image: linear-gradient(to bottom, #49BECB, #245E65)">
             <!--begin::Content-->
             <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
                 <!--begin::Logo-->
-                <a href="#" class="mb-0 mb-lg-12">
-                    <img alt="Logo" src="{{ asset('assets/media/logos/custom-1.png') }}" class="h-60px h-lg-75px">
+                <a href="#" class="mb-0 mb-lg-12 d-flex gap-5 align-items-center">
+                    <img alt="Logo" src="{{ asset('images/logo/Logo-magetan.png') }}" class="h-60px h-lg-80px rounded">
                 </a>
                 <!--end::Logo-->
 
                 <!--begin::Image-->
-                <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
-                    src="{{ asset('assets/media/misc/auth-screens.png') }}" alt="">
+                    <img style="height: 320px" src="{{ asset('assets/media/misc/auth-screens.png') }}" alt="" class="d-none d-lg-block mb-0 mb-lg-6">
                 <!--end::Image-->
 
                 <!--begin::Title-->
-                <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">
-                    Fast, Efficient and Productive
+                <h1 class="d-none d-lg-block text-white fs-1 fw-semibold text-center mb-1">
+                    SiCantik Platform Pengelolaan Kegiatan Pimpinan
                 </h1>
                 <!--end::Title-->
 
                 <!--begin::Text-->
-                <div class="d-none d-lg-block text-white fs-base text-center">
-                    In this kind of post, <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the
-                        blogger</a>
-
-                    introduces a person they’ve interviewed <br> and provides some background information about
-
-                    <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>
-                    and their <br> work following this is a transcript of the interview.
+                <div class="d-none d-lg-block text-white fs-5 text-center">
+                    Sistem Manajemen Acara ini mempermudah pengelolaan informasi kegiatan pimpinan, termasuk jadwal, penyimpanan undangan, dan riwayat kegiatan, untuk meningkatkan efisiensi dalam pengelolaan acara.
                 </div>
                 <!--end::Text-->
             </div>
